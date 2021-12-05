b7b39adf0a7348d77f23a233abd33c1a4f506e58
Mikayel Sargsyan <armlus75@gmail.com>
2021-12-05 17:53:53 +0400

    Added routing, logo and basic black background color. Made clicking on logo return to Home page.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
